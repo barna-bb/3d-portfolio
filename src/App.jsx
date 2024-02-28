ae6965b45a437d1ea3808305c649f6e429472f17
Barna <bbsoh00bb@gmail.com>
2024-02-28 18:22:37 +0000

    installed framer motion and added animatepresence to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { Home, About, Projects, Contact } from './pages/index';
 
+import { AnimatePresence } from 'framer-motion';
+
 function App() {
   return (
     <main className='bg-slate-300/20 h-full'>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/contact' element={<Contact />} />
-        </Routes>
+        <AnimatePresence mode="wait" initial={fake}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </AnimatePresence>
       </Router>
     </main>
   );
