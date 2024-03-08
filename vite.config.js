2401b3f2417332a0647ccc2bb30072b221455c1f
Barna <bbsoh00bb@gmail.com>
2024-03-08 13:49:39 +0000

    Config fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://barna-bb.github.io/3d-portfolio/",
-  assetsInclude: ['**/*.glb']
+  base: "https://barna-bb.github.io/3d-portfolio/"
 })
